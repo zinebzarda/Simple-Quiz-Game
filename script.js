f0efdfa57e2938385ef36f07cbf499b28dbb085a
zineb zarda <zibevzarda1@gmail.com>
2024-03-12 10:13:26 +0000

    add in code

---
+++ script.js
@@ -0,0 +1,41 @@
+const questions = [
+  {
+    question: "aaaaaaaaaa",
+    answers: [
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: true },
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: false },
+    ],
+  },
+  {
+    question: "aaaaaaaaaa",
+    answers: [
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: true },
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: false },
+    ],
+  },
+  {
+    question: "aaaaaaaaaa",
+    answers: [
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: true },
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: false },
+    ],
+  },
+  {
+    question: "aaaaaaaaaa",
+    answers: [
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: true },
+      { text: "bbb", correct: false },
+      { text: "bbb", correct: false },
+    ],
+  },
+];
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const next = document.getElementById("next");
